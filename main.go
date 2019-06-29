4bebf14e57c9eb0933dd5070b8360deb6991295a
Loric Brevet <loric.brevet@gmail.com>
2019-06-29 20:20:50 +0200

    Finish uploader

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -27,25 +28,40 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+	switch r.Method {
+	case "POST":
+		file, header, err := r.FormFile("file")
+		if err != nil {
+			fmt.Fprintf(w, "%v", err)
+			return
+		}
+		defer file.Close()
+
+		err = saveFile(file, header.Filename)
+		if err != nil {
+			fmt.Fprintf(w, "%v", err)
+			return
+		}
+
+		fmt.Fprintf(w, header.Filename)
+
+	case "DELETE":
+		file, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Fprintf(w, "%v", err)
+			return
+		}
+
+		if err := deleteFile(string(file)); err != nil {
+			fmt.Fprintf(w, "%v", err)
+			return
+		}
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		fmt.Fprintf(w, "%v", err)
-		return
-	}
-	defer file.Close()
-
-	err = saveFile(file, header.Filename)
-	if err != nil {
-		fmt.Fprintf(w, "%v", err)
-		return
-	}
-
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
 
 func logRequest(handler http.Handler) http.Handler {
@@ -56,13 +72,12 @@ func logRequest(handler http.Handler) http.Handler {
 }
 
 func saveFile(r io.Reader, name string) error {
-	dirPath, err := currentPath()
+	dirPath, err := uploadPath()
 	if err != nil {
 		return err
 	}
-	dirPath = filepath.Join(dirPath, "uploads")
 
-	// Make sure directory exists
+	// Make sure upload directory exists
 	_ = os.Mkdir(dirPath, 0755)
 
 	file, err := os.OpenFile(filepath.Join(dirPath, name), os.O_WRONLY|os.O_CREATE, 0664)
@@ -76,12 +91,27 @@ func saveFile(r io.Reader, name string) error {
 	return nil
 }
 
-func currentPath() (string, error) {
+func deleteFile(name string) error {
+	dirPath, err := uploadPath()
+	if err != nil {
+		return err
+	}
+
+	filePath := filepath.Join(dirPath, name)
+
+	if err := os.Remove(filePath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func uploadPath() (string, error) {
 	ex, err := os.Executable()
 	if err != nil {
 		return "", err
 	}
 
 	exPath := filepath.Dir(ex)
-	return exPath, nil
+	return filepath.Join(exPath, "uploads"), nil
 }
