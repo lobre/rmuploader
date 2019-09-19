117a597727c622bb145bee5f127be8a86020a119
Loric Brevet <loric.brevet@amersports.com>
2019-09-19 18:17:29 +0200

    Finish fixing error handling

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 
 	"github.com/PuerkitoBio/goquery"
@@ -48,14 +47,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	// TODO(lobre): GET should return the index page with the msg GET parameters processed
-	// TODO(lobre): POST should fetch from the url and upload. An error can directly be provided in the answer
 	http.HandleFunc("/", s.index)
-	// TODO(lobre): Ajax request to upload a file. In javascript, we should handle the error and redirect to / with msg.
-	// Another solution to avoid a redirect could be to directly change the error toast from javascript.
-	// That could allow to remove the GET parameter msg behavior from the / index page.
 	http.HandleFunc("/upload", s.upload)
-	// TODO(lobre): Ajax request to revert a file. In javascript, we should handle the error and redirect to / with msg.
 	http.HandleFunc("/delete", s.delete)
 
 	// Statics
@@ -69,92 +62,127 @@ func main() {
 }
 
 func (s server) index(w http.ResponseWriter, r *http.Request) {
-	msg := r.FormValue("msg")
 	tmpl := template.Must(template.ParseFiles("web/index.html"))
+	msg := ""
+
+	switch r.Method {
+	case "GET":
+	case "POST":
+		url := r.FormValue("url")
+		if url == "" {
+			msg = "url not provided"
+			break
+		}
+
+		file, err := webpageAsPDF(url)
+		if err != nil {
+			log.Fatal(err)
+			msg = err.Error()
+			break
+		}
+
+		name, err := titleFromURL(url)
+		if err != nil {
+			log.Fatal(err)
+			msg = err.Error()
+			break
+		}
+
+		id := uuid.New().String()
+		if err := s.uploadToRm(id, file, name); err != nil {
+			log.Fatal(err)
+			msg = err.Error()
+			break
+		}
+
+		w.WriteHeader(http.StatusOK)
+		msg = "webpage has been sent"
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		msg = http.StatusText(http.StatusMethodNotAllowed)
+	}
+
 	tmpl.Execute(w, msg)
 }
 
 func (s server) upload(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
+
+	// called using ajax
 	case "POST":
-		// Defines a new UUID for the document to be uploaded.
-		id := uuid.New().String()
-
-		if r.FormValue("file") == "" && r.FormValue("url") == "" {
-			http.Redirect(w, r, queryMsg("invalid form sent"), http.StatusFound)
+		if r.FormValue("file") == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "missing file parameter")
 			return
 		}
 
-		// if url is defined, we try to generate a PDF
-		// from the targeted website.
-		// Otherwise, we push the provided file.
-		url := r.FormValue("url")
-		if url != "" {
-			var err error
-			file, err := webpageAsPDF(url)
-			if err != nil {
-				http.Redirect(w, r, queryMsg(err.Error()), http.StatusFound)
-				return
-			}
-
-			name, err := titleFromURL(url)
-			if err != nil {
-				http.Redirect(w, r, queryMsg(err.Error()), http.StatusFound)
-				return
-			}
-
-			if err := s.uploadToRm(id, file, name); err != nil {
-				http.Redirect(w, r, queryMsg(err.Error()), http.StatusFound)
-				return
-			}
-
-			http.Redirect(w, r, queryMsg("document has been sent"), http.StatusFound)
-			return
-
-		} else {
-
-			fr, header, err := r.FormFile("file")
-			if err != nil {
-				http.Redirect(w, r, queryMsg(err.Error()), http.StatusFound)
-				return
-			}
-			defer fr.Close()
-
-			file, err := ioutil.ReadAll(fr)
-			if err != nil {
-				http.Redirect(w, r, queryMsg(err.Error()), http.StatusFound)
-				return
-			}
-
-			if err := s.uploadToRm(id, file, header.Filename); err != nil {
-				http.Redirect(w, r, queryMsg(err.Error()), http.StatusFound)
-				return
-			}
-
-			// send id as result
-			fmt.Fprintf(w, id)
-		}
-
-	case "DELETE":
-		id, err := ioutil.ReadAll(r.Body)
+		f, header, err := r.FormFile("file")
 		if err != nil {
-			http.Redirect(w, r, queryMsg(err.Error()), http.StatusFound)
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+		defer f.Close()
+
+		file, err := ioutil.ReadAll(f)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, err.Error())
 			return
 		}
 
-		if err := s.deleteFromRm(string(id)); err != nil {
-			http.Redirect(w, r, queryMsg(err.Error()), http.StatusFound)
+		id := uuid.New().String()
+		if err := s.uploadToRm(id, file, header.Filename); err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, err.Error())
 			return
 		}
+
+		// send id as result
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, id)
 
 	default:
-		http.Redirect(w, r, queryMsg("http method not supported"), http.StatusFound)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, http.StatusText(http.StatusMethodNotAllowed))
 		return
 	}
 
 }
 
 func (s server) delete(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+
+	// called using ajax
+	case "DELETE":
+		id, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+
+		if err := s.deleteFromRm(string(id)); err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+
+		// send id as result
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, string(id))
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, http.StatusText(http.StatusMethodNotAllowed))
+		return
+	}
 }
 
 func logRequest(handler http.Handler) http.Handler {
@@ -162,12 +190,6 @@ func logRequest(handler http.Handler) http.Handler {
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
 	})
-}
-
-// queryMsg returns a formatted url query string with
-// the msg parameter defined and encoded.
-func queryMsg(msg string) string {
-	return fmt.Sprintf("?msg=%s", url.QueryEscape(msg))
 }
 
 // titleFromURL parses an URL to return a
